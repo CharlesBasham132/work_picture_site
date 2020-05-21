702039f230bac4253df0d3ca3b1a6ce21627304d
Charles Basham <charliefred132@gmail.com>
2020-05-21 18:22:53 +0100

    started js file

---
+++ script.js
@@ -0,0 +1,21 @@
+// String, Numbers, Boolean, null, undefined
+
+// A method is a function accociated with an object
+
+// Arrays - variables that hold multiple values
+
+/* var, let, const  
+(var not commonly used anymore,
+let - reassignable value 
+const - value that does not change, cannot be reassigned,
+use const unless value is knowing to be changed.)
+*/
+// three === matches the data types, == matches just
+// value.
+
+const x = 10;
+if(x == 10) {
+    console.log('x is 10');
+}
+
+
