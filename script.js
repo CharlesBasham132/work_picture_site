145b83e884b5e6578cf9b7caa0e047c7db762e56
Charles Basham <charliefred132@gmail.com>
2020-05-24 18:22:44 +0100

    form validation completed

--- script.js
+++ script.js
@@ -13,9 +13,49 @@ use const unless value is knowing to be changed.)
 // three === matches the data types, == matches just
 // value.
 
-const x = 10;
-if(x == 10) {
-    console.log('x is 10');
-}
 
+/*
+Single element selectors:
+const form = document.getElementById('contact_form');
+console.log(form);
+
+console.log(document.querySelector('nav'))
+
+Multiple element selectors:
+console.log(document.querySelectorAll('h1'))
+*/
+
+/* The following are methods of removing elements:
+const email_box = document.querySelector('.text_box_2');    //assign name and select element
+email_box.remove();                                         //removes selected element
+*/
+
+
+//Form Script:
+const contact_form = document.querySelector('#contact_form');
+const first_name = document.querySelector('#fname');
+const last_name = document.querySelector('#lname');
+const email = document.querySelector('#email');
+const user_message = document.querySelector('#message_1');
+
+const error_message = document.querySelector('.error_message');
+const user_confirm = document.querySelector('#user_confirm');
+
+contact_form.addEventListener('submit', with_submit);
+
+function with_submit(e) {
+    e.preventDefault();
+ 
+    if(first_name.value === '' || last_name.value === '' || email.value === '' || user_message.value ==='') { 
+        error_message.innerHTML = 'ERROR: Please Enter All Fields';
+        setTimeout(() => error_message.remove(), 2000);
+    }
+    else {user_confirm.innerHTML = 'Form Submitted, Thank you ' + (`${first_name.value}`);
+    first_name.value = '';
+    last_name.value = '';
+    email.value = '';
+    user_message.value = '';
+    }
+}
+        
 
